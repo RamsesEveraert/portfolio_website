d4f5630486ee275418681275f9735c9cfc991ea5
RamsesEveraert <ramses.e@hotmail.be>
2024-07-11 11:29:26 +0200

    fixed bugs
    - changed homepage layout
    - projects filter changed: fits mobile screen, added active indicator

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,14 @@ document.addEventListener('DOMContentLoaded', function()
         var indicators = myCarousel.querySelectorAll('.custom-indicator');
         
         myCarousel.addEventListener('slide.bs.carousel', function (event) 
-        {
-            indicators.forEach(function (indicator) 
             {
-                indicator.classList.remove('active');
-            });
-            indicators[event.to].classList.add('active');
-        });
+                indicators.forEach(function (indicator) 
+                {
+                    indicator.classList.remove('active');
+                });
+                indicators[event.to].classList.add('active');
+            }
+        );
     }
 
     document.getElementById('contact-form').addEventListener('submit', function(event) 
@@ -26,42 +27,29 @@ document.addEventListener('DOMContentLoaded', function()
             }, function(error) 
             {
                 alert('Failed to send the message, please try again.');
-            });
+            }
+        );
     });
 });
-
-    
-    // Carousel indicators handling
-    var myCarousel = document.getElementById('carouselExampleCaptions');
-    if (myCarousel) {
-        var indicators = myCarousel.querySelectorAll('.custom-indicator');
-        
-        myCarousel.addEventListener('slide.bs.carousel', function (event) 
-        {
-            indicators.forEach(function (indicator) {
-                indicator.classList.remove('active');
-            });
-            indicators[event.to].classList.add('active');
-        });
-    }
 
     // Project filter handling
     const filterButtons = document.querySelectorAll('.filter-btn');
     const filterItems = document.querySelectorAll('.filter-item');
 
-    filterButtons.forEach(button => 
-        {
-        button.addEventListener('click', () => 
-            {
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Remove 'active' class from all buttons
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            // Add 'active' class to the clicked button
+            button.classList.add('active');
+
             const filter = button.getAttribute('data-filter');
 
             filterItems.forEach(item => {
                 const categories = item.getAttribute('data-category').split(' ');
-                if (filter === 'all' || categories.includes(filter)) 
-                    {
+                if (filter === 'all' || categories.includes(filter)) {
                     item.style.display = 'block';
-                } else 
-                {
+                } else {
                     item.style.display = 'none';
                 }
             });
